caa5b76f32f3bfd9ce95758843e2689bcd1ed8e4
Andrey Albaev <anemesttonami@gmail.com>
2025-09-28 17:45:59 +0300

    increase Owner version

--- src/test/java/org/rstqb/config/WebConfig.java
+++ src/test/java/org/rstqb/config/WebConfig.java
@@ -2,19 +2,23 @@ package org.rstqb.config;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources({"classpath:${isRemote}.properties"})
+@Config.Sources({"classpath:${env}.properties"})
 public interface WebConfig extends Config {
 
     @DefaultValue("https://%s:%s@selenoid.autotests.cloud/wd/hub")
+    @Key("selenoidUrl")
     String selenoidUrl();
 
     @DefaultValue("chrome")
+    @Key("browser")
     String browser();
 
     @DefaultValue("1920x1080")
+    @Key("browserSize")
     String browserSize();
 
     @DefaultValue("false")
+    @Key("isRemote")
     String isRemote();
 
     @DefaultValue("140.0")
@@ -22,9 +26,12 @@ public interface WebConfig extends Config {
     String browserVersion();
 
     @DefaultValue("https://www.rstqb.org/ru")
+    @Key("baseUrl")
     String baseUrl();
 
+    @Key("selenoidLogin")
     String selenoidLogin();
 
+    @Key("selenoidPass")
     String selenoidPass();
 }

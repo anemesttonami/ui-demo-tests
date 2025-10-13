cdd50c5b6a26f430923183d265cb5c1e99cdba32
Andrey Albaev <anemesttonami@gmail.com>
2025-10-13 20:02:27 +0300

    remove unnecessary @Key(...)

--- src/test/java/org/rstqb/config/WebConfig.java
+++ src/test/java/org/rstqb/config/WebConfig.java
@@ -6,36 +6,27 @@ import org.aeonbits.owner.Config;
 public interface WebConfig extends Config {
 
     @DefaultValue("https://%s:%s@selenoid.autotests.cloud/wd/hub")
-    @Key("selenoidUrl")
     String selenoidUrl();
 
     @DefaultValue("chrome")
-    @Key("browser")
     String browser();
 
     @DefaultValue("1920x1080")
-    @Key("browserSize")
     String browserSize();
 
     @DefaultValue("false")
-    @Key("isRemote")
     String isRemote();
 
     @DefaultValue("140.0")
-    @Key("browserVersion")
     String browserVersion();
 
     @DefaultValue("https://www.rstqb.org/ru")
-    @Key("baseUrl")
     String baseUrl();
 
-    @Key("selenoidLogin")
     String selenoidLogin();
 
-    @Key("selenoidPass")
     String selenoidPass();
 
     @DefaultValue("eager")
-    @Key("pageLoadStrategy")
     String pageLoadStrategy();
 }
